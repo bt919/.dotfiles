7dced875abce115a2e5916f25ae6c8602fb03ce4
Hisoka <gintama2009@hotmail.ca>
2025-05-22 21:35:39 +0000

    change line number color

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,37 @@
--- lua/plugins/rose-pine.lua
 return {
   "folke/tokyonight.nvim",
   config = function()
+    require("tokyonight").setup({
+      style = "moon",
+      light_style = "day",
+      transparent = false,
+      terminal_colors = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        sidebars = "dark",
+        floats = "dark",
+      },
+      day_brightness = 0.3,
+      dim_inactive = false,
+      lualine_bold = false,
+      on_colors = function(colors)
+        colors.fg_gutter = "#b2b8cf"
+      end,
+
+      on_highlights = function(highlights, colors) end,
+
+      cache = true,
+      plugins = {
+        all = package.loaded.lazy == nil,
+
+        auto = true,
+
+      },
+    })
+
     vim.cmd("colorscheme tokyonight-storm")
   end
 }
